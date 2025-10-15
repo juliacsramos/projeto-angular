03bfcbe525fee2245bfdb9af3489398f11404dbe
jujully <julia.ramos@iclass.com.br>
2025-10-15 12:08:30 -0300

    style: finalizando estudos de estilo

--- src/app/card-button/card-button.component.ts
+++ src/app/card-button/card-button.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-card-button',
   templateUrl: './card-button.component.html',
-  styleUrl: './card-button.component.scss'
-})
+  styleUrl: './card-button.component.scss',
+  //encapsulation: ViewEncapsulation.ShadowDom, // nenhum estilo de fora/global entra nesse componente, dificilmente vai precisar usar isso
+  encapsulation: ViewEncapsulation.Emulated, // padrão do angular
+  //encapsulation: ViewEncapsulation.None // remove a encapsulação desse componente
+}) // não existe um cenario que seja necessário usar isso
 export class CardButtonComponent {
 
 }
